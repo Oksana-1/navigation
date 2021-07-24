d09ee28c7159ba999b9d5c3dfeeb02cb39ef85d3
oksana-1 <4438488@gmail.com>
2021-07-24 15:36:11 +0300

    Bind 2 modules together

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav-list />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavList from "./components/NavList";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavList
   }
 }
 </script>
---
+++ src/components/NavList.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="mf-header">
+    <ul>
+      <li><a href="#">Dogs</a></li>
+      <li><a href="#">Dogs</a></li>
+      <li><a href="#">Dogs</a></li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "NavList"
+}
+</script>
+
+<style scoped>
+
+</style>
